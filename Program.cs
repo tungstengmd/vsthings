14eabd655db9a4bf7f8e59d8d399e8175306fcd7
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-01-07 11:51:12 +0000

    trans pride

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("trans pride 2025 <3");
+            Console.WriteLine("trans pride 2025\ni love you all, stay strong <3");
         }
     }
 }
