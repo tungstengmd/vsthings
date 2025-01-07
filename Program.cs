24df5faee29b1692b62263f186bb6557f291c829
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2025-01-07 11:44:21 +0000

    init :3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("trans pride 2025 <3");
         }
     }
 }
