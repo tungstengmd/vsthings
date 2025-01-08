40f3c06a18aba9f9bebe61b0e28d325405a76111
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-01-08 12:19:39 +0000

    bit more

--- Program.cs
+++ Program.cs
@@ -7,10 +7,12 @@
 			Console.WriteLine("trans pride 2025\ni love you all, stay strong <3");
 			Console.Write("wait, who are you btw ?\n# ");
 			string name = Console.ReadLine();
-			if (name == "harlow" || name == "harley" || name == "hailey") {
-                Console.WriteLine("oh, hai " + name + " ! where do i recognise that name ?");
+			if (name == "harlow" || name == "harley" || name == "hailey")
+			{
+				Console.WriteLine("oh, hai " + name + " ! where do i recognise that name ?");
+			} else { 
+				Console.WriteLine("oh, hai " + name + " !");
 			}
-			Console.WriteLine("oh, hai " + name + " !");
 		}
 	}
 }
