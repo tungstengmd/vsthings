e8e4987fcf6340a35c8852697ed6cc7ff5bb3fcf
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-01-08 12:18:52 +0000

    silly stuff :3

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("trans pride 2025\ni love you all, stay strong <3");
+			Console.Write("wait, who are you btw ?\n# ");
+			string name = Console.ReadLine();
+			if (name == "harlow" || name == "harley" || name == "hailey") {
+                Console.WriteLine("oh, hai " + name + " ! where do i recognise that name ?");
+			}
+			Console.WriteLine("oh, hai " + name + " !");
 		}
 	}
 }
