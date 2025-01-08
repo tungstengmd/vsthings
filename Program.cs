3a50624569989a19efbf5d9bd21fe7231e2d57c9
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-01-08 11:43:56 +0000

    tabs ??

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace vsIntro
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("trans pride 2025\ni love you all, stay strong <3");
-        }
-    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("trans pride 2025\ni love you all, stay strong <3");
+		}
+	}
 }
