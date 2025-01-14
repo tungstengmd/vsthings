87b7fc2ff428ff3a8438a825b14dbd133edf087a
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-01-14 11:47:14 +0000

    i never stage my changes unless i really need to x3

--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@
 			Thread.Sleep(new TimeSpan(0, 0, 1));
 			Console.Write("wait, who are you btw ?\n# ");
 			var name = Console.ReadLine();
-			if (name == "harlow" || name == "harley" || name == "hailey")
-			{
+			if (name == "harlow" || name == "harley" || name == "hailey") {
 				Console.WriteLine("oh, hai " + name + " ! where do i recognise that name ?");
 			} else { 
 				Console.WriteLine("oh, hai " + name + " !");
