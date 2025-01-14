605ffa564c78a171140fbfb3bad8b1876c78f105
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-01-14 11:31:55 +0000

    mew

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("trans pride 2025\ni love you all, stay strong <3");
+			Thread.Sleep(new TimeSpan(0, 0, 1));
 			Console.Write("wait, who are you btw ?\n# ");
-			string name = Console.ReadLine();
+			var name = Console.ReadLine();
 			if (name == "harlow" || name == "harley" || name == "hailey")
 			{
 				Console.WriteLine("oh, hai " + name + " ! where do i recognise that name ?");
